9b402051876feaec81a2c5a576d38b803db5f7e1
Peter Reintjes <peter.reintjes@ncmls.org>
2015-04-17 10:15:21 -0400

    Cellstat arduino code added

---
+++ host/param.h
@@ -0,0 +1,26 @@
+#define STANDALONE 1
+
+#define SAVE	1          // EEPROM STORAGE COMMANDS
+#define RESTORE	0
+
+#define ANALOG_TEMPERATURE   0 // Analog input for temperature
+#define ANALOG_TURBIDITY     2 // Analog input for turbidity
+
+#define LED             8     // Meniscus light
+#define LASER           7     // Meniscus light
+
+#define MIXER           6     // PWM for 12V motor
+#define HEATER          5     // On off for 48V Stovetop
+
+#define NUTRIENT        4     // Only one valve for Cellstat
+#define CYCLETIME       20
+
+//#define INDUCER1      4
+//#define HOSTCELLS     3
+//#define OUTFLOW       2
+
+#define MAX_VALVES      6
+
+#define MIXERSPEED 180   // PWM value for top mixer speed
+
+
---
+++ host/temperature.h
@@ -0,0 +1,79 @@
+#ifndef TEMPERATURE_v1_h
+#define TEMPERATURE_v1_h
+#define LIBRARY_VERSION	1.0.0
+
+#include "Arduino.h"
+#include "param.h"
+
+// Conversions between Analog In(A), Celcius (C), Farenheit (F)
+
+#define FULL_SCALE 1024  // Analog In 0-1023
+#define F_OFFSET 32.0
+#define F_SCALE_MULT 9.0
+#define F_SCALE_DIV 5.0
+#define C_TO_F(c) (F_OFFSET+(F_SCALE_MULT*c)/F_SCALE_DIV)
+#define F_TO_C(f) (F_SCALE_DIV*((f-F_OFFSET)/F_SCALE_MULT))
+#define A_TO_C(a) ((a*110)/1024.0)
+#define A_TO_F(a) C_TO_F(A_TO_C(a))
+
+// Constructors:
+// TEMPERATURE(A)       LM35-DZ analog temperature IC 10mV/C on pin A
+// TEMPERATURE(SCL,SDA) MELEXIS digital PIR, non-contact thermometer
+// Methods:
+// float c  = celcius()
+// float f  = farenheit()
+// float getScale()
+// float getOffset()
+// void setScale(float scale)
+// void setOffset(float offset)
+//
+
+class TEMPERATURE
+{
+ public:
+#define SAMPLES 10.0
+
+  TEMPERATURE(int pin) {
+    aIn = -1;
+    analogReference(INTERNAL);
+    sCL = 5;
+    sDA = 4;  
+  }
+
+  TEMPERATURE(int scl, int sda) {
+    aIn = -1;
+    analogReference(DEFAULT);
+    sCL = scl;
+    sDA = sda;
+  }
+
+  float farenheit(void)     { return C_TO_F(celcius()); }
+  float celcius(void)
+  {
+    int sum  = 0;
+    if (aIn != -1) { // Analog LM35DZ Temperature
+	for(int i=0; i< SAMPLES; i++) {
+		sum += analogRead(ANALOG_TEMPERATURE);
+		delay(2);
+	}
+	return A_TO_C(((float)sum)/SAMPLES);
+    } else { // Digital (Mexexis)
+      return (float) mlx.readObjectTempC();
+    }
+  }
+
+  float getScale(void)      { return scale;  }
+  float getOffset(void)     { return offset; }
+  void setScale(float sca)  { scale = sca;   }
+  void setOffset(float off) { offset = off;  }
+
+  private:
+   int aIn;            // Analog Input pin
+   int sCL;            // Melexis clock
+   int sDA;            // Melexis data
+   float scale;
+   float offset;
+   float avg;          // average accumulator
+  };
+#endif
+
