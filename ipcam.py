f300300cb60db48061a0e37d238c95894bd8bd92
eatoin5hrdlu <peter.reintjes@ncmls.org>
2015-05-02 23:31:18 -0400

    outdoor camera support

--- ipcam.py
+++ ipcam.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!C:/Python27/python -u
 import base64
 import time
 import urllib2
@@ -9,6 +9,11 @@ import numpy as np
 
 import os, subprocess
 import re
+# IPCamera module kknows about different cameras
+# Fluorescence module does color integration (returns saturation cycle)
+# Blob detection module ( find lagoon/cellstat coordinates)
+# import ipcam, fluor, blob
+import blob
 
 #outdoor = "00:62:6e:4f:17:d9"
 #indoor =  "c4:d6:55:34:8d:07"
@@ -24,31 +29,31 @@ import re
 
 
 class ipCamera(object):
+    """Color can be Blue=0, Green=1, or Red=2 (openCV => BGR, not RGB)
+       Image represents integration of [ selected color - 1/2(unselected colors) ]"""
 
     def __init__(self, ip_or_mac):
-        self.defaultIP = "172.16.3.101"    # Set this and we won't need to be superuser
+        self.defaultIP = "192.168.254.26" # IP for Windows or no superuser
+#            self.defaultIP = "172.16.3.101"
         self.ip = self.ValidIP(ip_or_mac)
-        if (self.ip == None):
+        if (self.ip == None) :
             print ip_or_mac, " is not a valid IP/MAC for a Camera"
             exit(1)
-        self.cameras = { 'outdoor' : "00:62:6e:4f:17:d9",
-                         'indoor'  : "c4:d6:55:34:8d:07" }
+        self.cameras = { "00:62:6e:4f:17:d9" : 'outdoor',
+                         "c4:d6:55:34:8d:07" : 'indoor' }
         self.camType = self.cameras.get(ip_or_mac,'indoor')
         self.picCmd = {'outdoor':":88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=admin&pwd=lakewould",
                        'indoor': "/snapshot.cgi?resolution=32&user=admin&pwd=lakewould" }
         self.url = "http://" + self.ip + self.picCmd[self.camType]
         self.normalBrightness = 100
         self.normalContrast = 4
-        self.brightnessCmd = {
-            'outdoor':"http://"+self.ip+":88/cgi-bin/CGIProxy.fcgi?cmd=setBrightness&brightness=",
-            'indoor' : "http://"+self.ip+"/camera_control.cgi?param=1&value=" }
-# NB: Foscam typo
-        self.contrastCmd = {
-            'outdoor': "http://"+self.ip+":88/cgi-bin/CGIProxy.fcgi?cmd=setContrast&constrast",
-            'indoor':  "http://"+self.ip+"/camera_control.cgi?param=2&value=" }
-        self.userpwd = {
-            'outdoor': "&usr=admin&pwd=lakewould",
-            'indoor':  "&user=admin&pwd=lakewould" }
+        self.brightnessCmd = {'outdoor':"http://"+self.ip+":88/cgi-bin/CGIProxy.fcgi?cmd=setBrightness&brightness=",
+                              'indoor' : "http://"+self.ip+"/camera_control.cgi?param=1&value=" }
+        # NB: Foscam typo
+        self.contrastCmd = {'outdoor': "http://"+self.ip+":88/cgi-bin/CGIProxy.fcgi?cmd=setContrast&constrast=",
+                            'indoor':  "http://"+self.ip+"/camera_control.cgi?param=2&value=" }
+        self.userpwd = {'outdoor': "&usr=admin&pwd=lakewould",
+                        'indoor':  "&user=admin&pwd=lakewould" }
 
         print "Using URL: " + self.url
         self.req = urllib2.Request(self.url)
@@ -74,6 +79,8 @@ class ipCamera(object):
         return ip
 
     def Mac2IP(self, mac) :
+        if os.name == 'nt' :
+            return self.defaultIP
         if not os.geteuid() == 0 :
             print "Superuser required to find IP from MAC, using default IP."
             return(self.defaultIP)
@@ -102,60 +109,82 @@ class ipCamera(object):
         except urllib2.URLError, msg :
             print msg, " Failed sending command to camera at ", self.ip
 
-# Color can be Blue=0, Green=1, or Red=2 (openCV => BGR, not RGB)
-# Image represents integration of [ selected color - 1/2(unselected colors) ]
-def showThisColor(color) :
-    frame = ipcam.grab()
-    picked = frame[:,:,color]  # Start with selected color image
-    while True:
-        temp =  ipcam.grab()
-        halfothers = cv2.addWeighted(temp[:,:,(color+1)%3], 0.55, temp[:,:,(color+2)%3], 0.55, 0 )
-        picked=cv2.addWeighted(picked,.85, cv2.subtract(temp[:,:,color],halfothers), 0.7, 0)
-        if not frame == None :
-            cv2.imshow("camera", picked)
-        if cv.WaitKey(10) == 27:
-            return
+    def showThisColor(color) :
+        frame = ipcam.grab()
+        picked = frame[:,:,color]  # Start with selected color image
+        while True:
+            temp =  ipcam.grab()
+            halfothers = cv2.addWeighted(temp[:,:,(color+1)%3], 0.5, temp[:,:,(color+2)%3], 0.5, 0 )
+#        picked=cv2.addWeighted(picked,0.9, cv2.subtract(temp[:,:,color],halfothers), 0.95, 0)
+            picked=cv2.add(picked,cv2.subtract(temp[:,:,color],halfothers))
+            if not frame == None :
+                labelImage(picked,color)
+                cv2.imshow("camera", picked)
+            if cv.WaitKey(10) == 27:
+                return
 
+    def labelImage(self, img, color) :
+        colors = {0:"blue", 1:"green", 2:"red" }
+        cv2.putText(img,colors[color],(10,80),cv2.FONT_HERSHEY_PLAIN,4.0,(240,80,200),2)
 
-# Narrow to each lagoon from initial blob detection
-def bioBlobs(color) :
-    frame = ipcam.grab()
-    picked = frame[:,:,color]  # Start with selected color image
-    cycle = 0
-    while True:
-        cycle = cycle + 1
-        temp =  ipcam.grab()
-        halfothers = cv2.addWeighted(temp[:,:,(color+1)%3], 0.55, temp[:,:,(color+2)%3], 0.55, 0 )
-        picked=cv2.addWeighted(picked,.85, cv2.subtract(temp[:,:,color],halfothers), 0.7, 0)
-        sat = 0
-        x1 = 500
-        x2 = 600
-        y1 = 350
-        y2 = 450
-        total = (x2-x1)*(y2-y1)
-        sat = cv2.countNonZero(cv2.subtract(picked[y1:y2,x1:x2], 250))
-        cv2.rectangle(picked,(x1,y1),(x2,y2),255)
-#        for i in range(rows) :
-#            for j in range(cols) :
-#                if (picked[i,j] > 250) :
-#                    sat = sat + 1
-        print str(sat) + " saturated out of " + str(total) + " at cycle " + str(cycle)
-        if not frame == None :
-            cv2.imshow("camera", picked)
-        if cv.WaitKey(10) == 27:
-            return
+    def bioBlobs(self, color, (x1,y1,x2,y2)) :
+        frame = None
+        while(frame == None) :
+            frame = ipcam.grab()
+        picked = frame[:,:,color]  # Start with selected color image
+        cycle = 0
+        while True:
+            cycle = cycle + 1
+            temp = None
+            tries = 0
+            while (temp == None and tries < 10) :
+                temp =  ipcam.grab()
+                if (temp == None) :
+                    print "Failed to get image from camera"
+                    tries = tries + 1
+                    time.sleep(1000)
+            if (temp == None) :
+                print "Giving up on camera connection"
+                return None
+                
+            halfothers = cv2.addWeighted(temp[:,:,(color+1)%3], 0.5, temp[:,:,(color+2)%3], 0.5, 0 )
+            picked=cv2.addWeighted(picked,0.9, cv2.subtract(temp[:,:,color],halfothers), 0.9, 0)
+            sat = 0
+            total = (x2-x1)*(y2-y1)
+            lit = cv2.countNonZero(cv2.subtract(picked[y1:y2,x1:x2], 128))
+            sat = cv2.countNonZero(cv2.subtract(picked[y1:y2,x1:x2], 250))
+            print str(sat) + " saturated " + str(lit) + " detected out of " + str(total) + " at cycle " + str(cycle)
+            cv2.rectangle(picked,(x1,y1),(x2,y2),255)
+            if not frame == None :
+                self.labelImage(picked,color)
+                cv2.imshow("camera", picked)
+            if cv.WaitKey(10) == 27:
+                return
 
 if __name__ == "__main__" :
+    lagoon_position = { 'Lagoon1' : (200,500,300,700),
+                        'Lagoon2' : (400,500,500,700),
+                        'Lagoon3' : (700,500,800,700),
+                        'Lagoon4' : (900,500,1000,700) }
     outdoor = "00:62:6e:4f:17:d9"
     indoor = "c4:d6:55:34:8d:07"
-    ipcam = ipCamera(indoor)
-    br = 100  # 0-240 for indoor
-    co = 4    # 0-6 for indoor
-    ipcam.brightness(br);
-    ipcam.contrast(co);
+    ipcam = ipCamera(outdoor)
+    b = blob.Blob(2) # A blob detector for green(1) blobs
+    br = 80  # 0-240 for indoor (ptz 905) 0-100 for outdoor (910)
+    co = 50    # 0-6 for indoor  0-100 for outdoor
+    ipcam.brightness(br)
+    ipcam.contrast(co)
     cv2.namedWindow("camera", cv2.CV_WINDOW_AUTOSIZE)
-    cv2.moveWindow("camera", 20, 10)
-    bioBlobs(1)
-    showThisColor(0)
-    showThisColor(1)
-    showThisColor(2)
+    frame = ipcam.grab()
+    bbs = b.blobs(frame)
+    cv2.imshow("camera", frame)
+    if cv.WaitKey(4000) == 27:
+	exit()
+    ipcam.bioBlobs(2,lagoon_position['Lagoon1'])
+    ipcam.bioBlobs(1,lagoon_position['Lagoon2'])
+    ipcam.bioBlobs(1,lagoon_position['Lagoon3'])
+    ipcam.bioBlobs(0,lagoon_position['Lagoon4'])
+#    bioBlobs(1)
+#    showThisColor(0)
+#    showThisColor(1)
+#    showThisColor(2)
