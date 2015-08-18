f8eafee3f14d305dba4b846538e84ff8b0b5ea7c
eatoin5hrdlu <peter.reintjes@ncmls.org>
2015-08-17 22:25:43 -0400

    sscanf added

--- wifi/wifi.h
+++ wifi/wifi.h
@@ -18,7 +18,15 @@
 #endif
 
 /*
- * Wifi submodule (meant to be plug-in alternative for Bluetooth)
+ * Wifi submodule (plug-in alternative for Bluetooth)
+ *
+ * 1) Configure ESP8266 WiFi connection server and wait to be contacted
+ *    WIFI w = WIFI();
+ *         w.start_server();    
+ *
+ * 2) Accept connection and respond to requests
+ *         char *cp = w.recv()    returns pointer to string buffer
+ *         w.send(char buf[])     sends text addressed in buf
  */
 
 class WIFI
@@ -74,30 +82,39 @@ class WIFI
       int token,length,idx;
       if (readstring()) 
 	{
-	  if (!strncmp(buf,"+IDP=",5)) {
-	    int token,length;
-	    int idx = 5;
-	    lastid = buf[idx]-'0';
-	    idx++;
-	    if (buf[idx] != ',') {
-	      return mysend("error1");
+	  char *cp = "+IDP=0,12:abcdefghijklm";
+	  if ( sscanf(buf,"+IDP=%d,%d:%s",&lastid,&length,&buf) == 3 )
+	    {
+	      Serial.println("Got it");
 	    }
-	    idx++;
-	    token = idx;
-	    while(buf[idx] != ',') idx++;
-	    buf[idx] = 0;
-	    length = atoi(&buf[token]);
-	    idx++;
-	    if (buf[idx+length-1] != 10 || buf[idx+length-2] != 13) {
-	      return mysend("error2");
-	    } else {
-	      mysend("Data was not properly terminated");
+	  else
+	    {
+	      if (!strncmp(buf,"+IDP=",5)) {
+		int token,length;
+		int idx = 5;
+		lastid = buf[idx]-'0';
+		idx++;
+		if (buf[idx] != ',') {
+		  return mysend("error1");
+		}
+		idx++;
+		token = idx;
+		while(buf[idx] != ',') idx++;
+		buf[idx] = 0;
+		length = atoi(&buf[token]);
+		idx++;
+		if (buf[idx+length-1] != 10 || buf[idx+length-2] != 13) {
+		  return mysend("error2");
+		} else {
+		  mysend("Data was not properly terminated");
+		}
+		buf[idx+length-2] = 0;
+		return(&buf[idx]);
+	      }
 	    }
-	    buf[idx+length-2] = 0;
-	    return(&buf[idx]);
-	  }
 	}
     }
+	
 
   void respondToRequest(void)
   {
@@ -132,6 +149,6 @@ class WIFI
     Serial.println("AT+CIPSERVER=1,23");
   }
 
-};  // End of Wifi Class
+    };  // End of Wifi Class
 #endif
 
